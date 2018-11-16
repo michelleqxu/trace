a746113b7ce077fcc766906e708dd12de799c875
Shea <sheaconlon@users.noreply.github.com>
2018-11-15 17:18:15 -0800

    add data stores and entry access methods

---
+++ app/src/main/java/org/cs160/fa18/group12/traces/Entry.java
@@ -0,0 +1,47 @@
+package org.cs160.fa18.group12.traces;
+
+public class Entry {
+    /* A journal entry. */
+
+    private long ts;
+    private float severity;
+    private String cause;
+    private String note;
+
+    public static Entry fromString(String serialized) {
+        /* Deserialize this entry from a string.
+         *  serialized: The serialized entry.
+         */
+        String[] split = serialized.split("\\|");
+        if (split.length != 4) {
+            throw new RuntimeException("You messed up Shea.");
+        }
+        return new Entry(
+                Long.parseLong(split[0]),
+                Float.parseFloat(split[1]),
+                split[2],
+                split[3]
+        );
+    }
+
+    public Entry(long ts, float severity, String cause, String note) {
+        /* Create an entry.
+         *  ts: The timestamp at which the entry was created.
+         *  severity: The severity of the entry.
+         *  cause: The name of the cause of the entry.
+         *  note: The content of the note attached to the entry. Must not contain pipe characters.
+         */
+        this.ts = ts;
+        this.severity = severity;
+        this.cause = cause;
+        if (note.contains("|")) {
+            throw new RuntimeException("Note contains pipe characters.");
+        }
+        this.note = note;
+    }
+
+    public String toString() {
+        /* Serialize this entry to a string. */
+        return ts + "|" + severity + "|" + cause + "|" + note;
+    }
+}
--- app/src/main/java/org/cs160/fa18/group12/traces/MainActivity.java
+++ app/src/main/java/org/cs160/fa18/group12/traces/MainActivity.java
@@ -1,17 +1,22 @@
 package org.cs160.fa18.group12.traces;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.constraint.ConstraintLayout;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.TextView;
+
+import java.util.HashSet;
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mTextMessage;
+    private SharedPreferences settingStore;
+    private SharedPreferences entryStore;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -63,8 +68,22 @@ public class MainActivity extends AppCompatActivity {
         // Add the onclick handler to the navigation bar.
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        // Initialize the data stores.
+        settingStore = getSharedPreferences("settings", MODE_PRIVATE);
+        entryStore = getSharedPreferences("entries", MODE_PRIVATE);
+
+        // Store and read back some dummy data (just to test).
+        Set<Entry> entries = new HashSet<Entry>();
+        entries.add(new Entry(100l, 0.5f, "bla", "note"));
+        entries.add(new Entry(200l, 0.8f, "bla2", "note2"));
+        setEntries(entries);
+        Log.d("bla", getEntries().toString());
     }
 
+    /* ********************************************
+     * Layout/view getters, purely for convenience.
+     * ********************************************/
     private ConstraintLayout getHistoryContainer() {
         /* Gets the history_container. */
         return (ConstraintLayout) findViewById(R.id.history_container);
@@ -76,8 +95,47 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private ConstraintLayout getSettingsContainer() {
-        /* Gets the settings_container. */
+        /* Gets the settings_container.
+        * */
         return (ConstraintLayout) findViewById(R.id.settings_container);
+    }
+
+    /* *******************************
+     * Data store interaction methods.
+     * *******************************/
+    private Set<Entry> getEntries() {
+        /* Get the entries from the data store. Note that changes to the returned set will NOT be
+         *  persisted in the data store.
+         */
+        // Retrieve the serialized entries from the data store.
+        Set<String> serializedEntries = entryStore.getStringSet("entries", null);
+        if (serializedEntries == null) {
+            serializedEntries = new HashSet<String>();
+        }
+
+        // Deserialize them.
+        Set<Entry> entries = new HashSet<Entry>();
+        for (String serialized : serializedEntries) {
+            entries.add(Entry.fromString(serialized));
+        }
+
+        return entries;
+    }
+
+    private void setEntries(Set<Entry> entries) {
+        /* Set the entries in the data store.
+         *  entries: The entries to store.
+         */
+        // Serialize them.
+        Set<String> serializedEntries = new HashSet<String>();
+        for (Entry entry : entries) {
+            serializedEntries.add(entry.toString());
+        }
+
+        // Store the serialized entries into the data store.
+        SharedPreferences.Editor editor = entryStore.edit();
+        editor.putStringSet("entries", serializedEntries);
+        editor.apply();
     }
 
 }
