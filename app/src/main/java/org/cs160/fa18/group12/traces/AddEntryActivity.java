5fe8698298c73d6944f01b8200efeb180abe498d
Shea <sheaconlon@users.noreply.github.com>
2018-11-15 17:59:01 -0800

    prototype add entry activity

---
+++ app/src/main/java/org/cs160/fa18/group12/traces/AddEntryActivity.java
@@ -0,0 +1,13 @@
+package org.cs160.fa18.group12.traces;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddEntryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_entry2);
+    }
+}
